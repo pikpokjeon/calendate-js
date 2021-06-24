159b6fab90c4b3ab41013cd887139c6d54ac7210
pikpokjeon <pikpokjeon@gmail.com>
2021-06-24 11:59:46 +0900

    feature: store 상태관리  함수

---
+++ src/lib/store.js
@@ -0,0 +1,31 @@
+const Store = (initialState) =>
+{
+    let innerState = {
+        subscribers: [],
+        state: Object.assign({}, initialState)
+    }
+
+    const notify = () =>
+    {
+        innerState.subscribers.forEach(sub => sub());
+    }
+
+    const getState = () => Object.assign({}, innerState.state)
+
+    const setState = stateUpdates =>
+    {
+        innerState.state = Object.assign(Object.assign({}, innerState.state), stateUpdates);
+        notify();
+    }
+    const action = f => (...args) => setState(f(...args, getState()) ?? {})
+    const subscribe = (sub) =>
+    {
+        innerState.subscribers.push(sub);
+        notify();
+    };
+
+    return { subscribe, getState, setState, action }
+}
+
+
+export { Store }
