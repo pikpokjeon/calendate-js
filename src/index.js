395f2f5d55a0f0494d7c4db103a9660ab38761d2
pikpokjeon <pikpokjeon@gmail.com>
2021-06-24 13:50:46 +0900

    refactor: 날짜 스토어 초기화 부분 초기 데이터 적용

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import { App } from './app.js';
 import { renderTo } from './lib/lib.js'
 import { Store } from './lib/store.js'
-import { getToday } from './lib/date.js';
+import { getWeekDay, getToday, getCalendar } from './lib/date.js';
 
-const today = getToday()
 
-const store = Store({
-    lists: [],
-    selected: [],
-    month: today.month,
-    year: today.year,
-    dates: today.dates,
-})
+const initDate = () =>
+{
+    const today = getToday()
+    const weekDate = getWeekDay(today.dates)
+    const lists = getCalendar(today.dates, today.month, 1, [], weekDate.prefix)
 
-store.subscribe(
+    const store = Store({
+        lists,
+        selected: [],
+        month: today.month,
+        year: today.year,
+        dates: today.dates,
+    })
+
+    return { store }
+}
+
+const dateStore = initDate().store
+
+dateStore.subscribe(
     () => renderTo(
         document.querySelector("#app"),
-        App(store)
+        App(dateStore.store)
     )
 )
