a4dd0ec4ddaee956d9c6908b116e2a14aa4e06a6
Yaejin Beatrix Jeon <59504386+pikpokjeon@users.noreply.github.com>
2021-07-06 03:37:08 +0900

    refactor: 프리티어 적용

--- src/lib/store.js
+++ src/lib/store.js
@@ -5,19 +5,18 @@ const Store = (initialState) =>
         state: Object.assign({}, initialState)
     }
 
-    const notify = () =>
-    {
-        innerState.subscribers.forEach(sub => sub());
-    }
-
+    const notify = () => innerState.subscribers.forEach(sub => sub());
+   
     const getState = () => Object.assign({}, innerState.state)
+    
     const setState = stateUpdates =>
     {
         innerState.state = Object.assign(Object.assign({}, innerState.state), stateUpdates);
-        console.log(innerState.state)
         notify();
     }
+    
     const action = f => (...args) => setState(f(...args, getState()) ?? {})
+    
     const subscribe = (sub) =>
     {
         innerState.subscribers.push(sub);
